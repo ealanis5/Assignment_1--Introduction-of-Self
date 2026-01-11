c4795331b7ea4490f4617b2a45cf3905ef67b331
ealanis5 <ealanis5@student.rccd.edu>
2026-01-10 20:48:08 -0800

    Added new information/code

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main() {
+    std::string name = "Erickberto Alanis";
+    double height = 5.7;
+    std::cout << "Name: " << name << ", Height: " << height << "m" << std::endl;
+    std::string age = " 17 years old";
+    std::string goals = "Become a chemical engineer/physician in the future.";
+    std::cout << "Age: " << age << ", goals: " << goals << std::endl;
+    std::string interests = " Learning Science, finsihing school, and having fun.";
+    std::string dreams = "Become a chemical engineer/physician in the future. Along with owning a house and a successful buisness.";
+    std::cout << "Interests: " << interests << ", Dreams: " << dreams << std::endl;
+    std::string One_year = " I see myself finishing high school and starting college.";
+    std::string Three_years = " Hopefully graduating college with a degree in chemical engineering/ pre-med.";
+    std::cout << "One_year: " << One_year << ", Three_years: " << Three_years << std::endl;
+    std::string Five_years = " I see myself in medical school or working as a chemical engineer.";
+    std::string Seven_years = " I hope to see myself graduating medical school or had done something great in the field of chemical engineering.";
+    std::cout << "Five_years: " << Five_years << ", Seven_years: " << Seven_years << std::endl;
+    return 0;
+}
+
+#include <iostream>
+int main() {
+    std::string age = " 17 years old";
+    std::string goals = "Become a chemical engineer/physician in the future.";
+    std::cout << "Age: " << age << ", goals: " << goals << std::endl;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "17 years old: ";  //Output using cout
+   cin >> age;                 //Input using cin
+    cout << "You are " << age << " years old!" << endl;
+    return 0;
+}
